6d3676087e1aaddd20eb504cae02935356888a6e
Andreas Berheim Brudin <andreas.brudin@vargasholding.se>
2022-12-08 20:36:10 +0100

    Refine day 5

--- src/main/kotlin/Day5.kt
+++ src/main/kotlin/Day5.kt
@@ -1,39 +1,31 @@
 fun day5(input: String): Pair<String, String> {
-    data class Move(val moveAmount: Int, val from: Int, val to: Int)
-    data class Stacks(val value: List<List<Char>>) {
-        constructor(size: Int) : this(1.rangeTo(size).map { listOf<Char>() })
+    data class Move(val amount: Int, val from: Int, val to: Int)
 
-        fun topFromEach(): String = value.map { it.last() }.joinToString("")
+    val (initStacks, initMoves) = input.split("\n\n").map { it.trimEnd() }
+    val numStacks = initStacks.last().digitToInt()
 
-        fun makeMoveA(move: Move): Stacks =
-            Stacks(1.rangeTo(move.moveAmount).fold(value) { stacks, _ -> makeMove(stacks, move, 1) })
+    val stacks = initStacks.split("\n").dropLast(1).map { it.chunked(4).map { it[1] } }
+        .fold(List(numStacks) { listOf<Char>() }) { stacks, row ->
+            stacks.mapIndexed { index, value -> value + row.getOrElse(index) { ' ' } }
+        }.map { it.reversed().takeWhile { it != ' ' } }
 
-        fun makeMoveB(move: Move): Stacks = Stacks(makeMove(value, move, move.moveAmount))
+    val moves = initMoves.split("\n").map {
+        val (amount, from, to) = "move (\\d+) from (\\d+) to (\\d+)".toRegex().matchEntire(it)!!.destructured
+        Move(amount.toInt(), from.toInt() - 1, to.toInt() - 1)
+    }
 
-        private fun makeMove(stacks: List<List<Char>>, move: Move, amount: Int) = stacks.withIndex().map {
-            when (it.index) {
-                move.from -> it.value.dropLast(amount)
-                move.to -> it.value + stacks[move.from].takeLast(amount)
-                else -> it.value
-            }
+    fun move(stacks: List<List<Char>>, move: Move, amount: Int): List<List<Char>> = stacks.mapIndexed { index, value ->
+        when (index) {
+            move.from -> value.dropLast(amount)
+            move.to -> value + stacks[move.from].takeLast(amount)
+            else -> value
         }
     }
 
-    val parsed = input.split("\n\n").map { it.split("\n").dropLast(1) }
-    val numStacks = parsed[0].last().split(" ").size
-
-    val stacks = parsed[0].reversed().map { it.chunked(4).map { it[1] } }
-        .fold(Stacks(numStacks)) { stacks, row ->
-            Stacks(stacks.value.withIndex().map {
-                if (row.getOrElse(it.index) { ' ' } != ' ') it.value + row[it.index] else it.value
-            })
-        }
-
-    val moves = parsed[1].map { it.split(" ").chunked(2).map { it[1].toInt() } }
-        .map { Move(moveAmount = it[0], from = it[1] - 1, to = it[2] - 1) }
-
     return Pair(
-        moves.fold(stacks) { prevStacks, move -> prevStacks.makeMoveA(move) }.topFromEach(),
-        moves.fold(stacks) { prevStacks, move -> prevStacks.makeMoveB(move) }.topFromEach()
+        moves.fold(stacks) { prev, move -> (1..move.amount).fold(prev) { stacks, _ -> move(stacks, move, 1) } }
+            .map { it.last() }.joinToString(""),
+        moves.fold(stacks) { prevStacks, move -> move(prevStacks, move, move.amount) }
+            .map { it.last() }.joinToString("")
     )
 }
