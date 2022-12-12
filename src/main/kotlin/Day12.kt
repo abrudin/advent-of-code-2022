a9ad21259cc085cee8e88438008c4df6295e6bfa
Andreas Berheim Brudin <andreas.brudin@vargasholding.se>
2022-12-12 18:33:10 +0100

    Tiny improvement Day 12

--- src/main/kotlin/Day12.kt
+++ src/main/kotlin/Day12.kt
@@ -4,17 +4,17 @@ fun day12(input: String): Pair<Int, Int> {
     val rows = tiles.length / cols
     val mapped = tiles.map { if (it == 'S') 'a' else if (it == 'E') 'z' else it }
 
-    data class Hill(val previous: Set<Int>, val current: Set<Int>, val steps: Int = 0)
+    data class Hill(val current: Set<Int>, val previous: Set<Int> = setOf(), val steps: Int = 0)
 
-    fun stepsUntilEnd(init: Set<Int>): Int = generateSequence(Hill(setOf(), init)) { hill ->
-        Hill(hill.previous + hill.current, hill.current.flatMap { i ->
+    fun stepsUntilEnd(init: Set<Int>): Int = generateSequence(Hill(init)) { hill ->
+        Hill(hill.current.flatMap { i ->
             listOfNotNull(
                 if (i >= cols) i - cols else null, // Up
                 if (i < (rows - 1) * cols) i + cols else null, // Down
                 if (i % cols > 0) i - 1 else null, // Left
                 if (i % cols < (cols - 1)) i + 1 else null // Right
             ).filter { !hill.previous.contains(it) && mapped[it] <= mapped[i].inc() }
-        }.toSet(), hill.steps.inc())
+        }.toSet(), hill.previous + hill.current, hill.steps.inc())
     }.dropWhile { !it.current.contains(tiles.indexOf('E')) }.first().steps
 
     return Pair(
