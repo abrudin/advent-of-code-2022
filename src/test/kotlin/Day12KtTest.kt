ed2b7e71a4616c4bce1ac78ece4581b03b51238d
Andreas Berheim Brudin <andreas.brudin@vargasholding.se>
2022-12-12 17:31:15 +0100

    Day 12

---
+++ src/test/kotlin/Day12KtTest.kt
@@ -0,0 +1,15 @@
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+
+internal class Day12KtTest {
+    @Test
+    fun test() {
+        val (exampleA, exampleB) = day12(AocUtils.loadResourceAsString("day12-example.txt"))
+        assertThat(exampleA).isEqualTo(31)
+        assertThat(exampleB).isEqualTo(29)
+
+        val (realA, realB) = day12(AocUtils.loadResourceAsString("day12.txt"))
+        assertThat(realA).isEqualTo(468)
+        assertThat(realB).isEqualTo(459)
+    }
+}
