a8a5f3df8b0def63a8e27c5a5408b722be1c33b7
Andreas Berheim Brudin <andreas.brudin@vargasholding.se>
2022-12-13 22:27:39 +0100

    Clean up solution

--- src/main/kotlin/Day13.kt
+++ src/main/kotlin/Day13.kt
@@ -3,7 +3,23 @@ sealed interface Parser {
         val parent: Node? = null,
         val children: MutableList<Parser> = mutableListOf(),
         val running: MutableList<Char> = mutableListOf()
-    ) : Parser
+    ) : Parser {
+        fun addLeaf(returnParent: Boolean = false): Node {
+            if (running.size > 0) children.add(Leaf(running.joinToString("").toInt()))
+            running.clear()
+            return if (returnParent) parent!! else this
+        }
+
+        fun addNode(): Node {
+            children.add(Node(this))
+            return children.last() as Node
+        }
+
+        fun addToken(token: Char): Node {
+            running.add(token)
+            return this
+        }
+    }
 
     data class Leaf(val i: Int) : Parser
 
@@ -17,60 +33,40 @@ sealed interface Parser {
         fun parse(input: String) =
             input.fold(Node()) { acc, token ->
                 when (token) {
-                    '[' -> {
-                        acc.children.add(Node(acc)); acc.children.last() as Node
-                    }
-
-                    ']' -> {
-                        if (acc.running.size > 0) acc.children.add(Leaf(acc.running.joinToString("").toInt()))
-                        acc.running.clear(); acc.parent!!
-                    }
-
-                    ',' -> {
-                        if (acc.running.size > 0) acc.children.add(Leaf(acc.running.joinToString("").toInt()))
-                        acc.running.clear(); acc
-                    }
-
-                    else -> {
-                        acc.running.add(token); acc
-                    }
+                    '[' -> acc.addNode()
+                    ']' -> acc.addLeaf(returnParent = true)
+                    ',' -> acc.addLeaf()
+                    else -> acc.addToken(token)
                 }
             }.asPacket()
     }
 }
 
 sealed interface Packet : Comparable<Packet> {
-    fun <T> List<T>.zipFill(other: List<T>, fill: T): List<Pair<T, T>> =
-        (this + if (other.size > this.size) List(other.size - this.size) { fill } else listOf())
-            .zip(other + if (this.size > other.size) List(this.size - other.size) { fill } else listOf())
-
-    override operator fun compareTo(other: Packet): Int =
-        when (this) {
-            is Leaf -> when (other) {
-                is Leaf -> this.i.compareTo(other.i)
-                is Node -> if (this.i < 0) -1 else Node(listOf(this)).compareTo(other)
+    data class Node(val children: List<Packet>) : Packet {
+        override fun compareTo(other: Packet): Int =
+            when (other) {
+                is Leaf -> compareTo(Node(listOf(other)))
+                is Node -> children.zip(other.children)
+                    .map { it.first.compareTo(it.second) }
+                    .find { it != 0 } ?: children.size.compareTo(other.children.size)
             }
+    }
 
-            is Node -> when (other) {
-                is Leaf -> if (other.i < 0) 1 else this.compareTo(Node(listOf(other)))
-                is Node -> this.children.zipFill(other.children, Leaf(-1))
-                    .map { it.first.compareTo(it.second) }.find { it != 0 } ?: 0
+    data class Leaf(val i: Int) : Packet {
+        override fun compareTo(other: Packet): Int =
+            when (other) {
+                is Leaf -> i.compareTo(other.i)
+                is Node -> Node(listOf(this)).compareTo(other)
             }
-        }
-
-
-    data class Node(val children: List<Packet>) : Packet
-
-    data class Leaf(val i: Int) : Packet
+    }
 }
 
 fun day13(input: String): Pair<Int, Int> {
     val all = input.split("\n\n").flatMap { it.split("\n") }.map(Parser.Companion::parse)
-    val divPacket1: Packet = Packet.Node(listOf(Packet.Node(listOf(Packet.Leaf(2)))))
-    val divPacket2: Packet = Packet.Node(listOf(Packet.Node(listOf(Packet.Leaf(6)))))
-    val allWithDividers = ((all + divPacket1) + divPacket2).sorted().toList()
+    val divPackets = listOf("[[2]]", "[[6]]").map(Parser.Companion::parse)
     return Pair(
         all.chunked(2).withIndex().filter { it.value[0] < it.value[1] }.sumOf { it.index + 1 },
-        listOf(divPacket1, divPacket2).map(allWithDividers::indexOf).map { it.inc() }.fold(1) { a, b -> a * b }
+        divPackets.map((all + divPackets).sorted()::indexOf).map { it.inc() }.fold(1) { a, b -> a * b }
     )
 }
